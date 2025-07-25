c6a15cf082b5ff16b53e1a3f5cb571c1d68a3103
Pedr00ga <plgonzales4760@gmail.com>
2025-07-25 16:48:46 -0300

    Get method implemented and tested

--- src/main/java/br/com/treinow/controller/UserController.java
+++ src/main/java/br/com/treinow/controller/UserController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -23,6 +25,19 @@ public class UserController {
         var userEntity = new UserEntity();
         BeanUtils.copyProperties(userDto, userEntity);
         return ResponseEntity.status(HttpStatus.CREATED).body(userJpa.save(userEntity));
+
+    }
+    @GetMapping("/users")
+    public ResponseEntity<List<UserEntity>> getAllUsers(){
+        return ResponseEntity.status(HttpStatus.OK).body(userJpa.findAll());
     }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value="id") UUID id){
+        Optional<UserEntity> userO = userJpa.findById(id);
+        if(userO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userO.get());
+    }
 }
