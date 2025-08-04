6ec1555d2183332cb7ac33cbc9fae3ea4910ece8
Pedr00ga <plgonzales4760@gmail.com>
2025-08-04 11:49:31 -0300

    Implemented and tested updated and delete in Membership controller

--- src/main/java/br/com/treinow/controller/MembershipController.java
+++ src/main/java/br/com/treinow/controller/MembershipController.java
@@ -4,6 +4,7 @@ import br.com.treinow.dtos.MembershipDto;
 import br.com.treinow.models.entities.MembershipEntity;
 import br.com.treinow.service.MembershipService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Member;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/membership")
@@ -30,6 +32,27 @@ public class MembershipController {
     @ResponseStatus(HttpStatus.OK)
     public List<MembershipEntity> getAllMembership(){
         return membershipService.getAllMembership();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateMembership(@PathVariable UUID id,
+                                                   @RequestBody @Valid MembershipDto membershipDto){
+        try{
+            var membershipUpdated = membershipService.updateMembership(id, membershipDto);
+            return ResponseEntity.status(HttpStatus.OK).body(membershipUpdated);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Membership not found verify id");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteMembership(@PathVariable UUID id){
+        try{
+            membershipService.deleteMembership(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Membership delete sucessfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Membership not found verify id");
+        }
     }
     
 }
